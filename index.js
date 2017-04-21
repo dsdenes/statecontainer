695b06d26b5a044463fe1a1e45e471d03d8284f3
Denes Pal <denes.pal@lab.coop>
2017-04-21 13:40:42 +0200

    refactor: Make it more readable
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

--- index.js
+++ index.js
@@ -5,31 +5,48 @@ const {
   cachedResult
 } = require('./units');
 
-const getter = store => (path, defaultValue) => {
-  return (typeof path === 'undefined') ? store : _.get(store, path, defaultValue);
-};
-const setter = store => (path, value) =>  _.set(store, path, value);
-const haser = store => path =>  _.has(store, path);
-const pusher = store => value => store.push(value);
-
-const storeCreator = store => (path, initial={}) => {
-  if (!_.has(store, path)) {
-    _.set(store, path, initial);
+function getter(store) {
+  return (path, defaultValue) => {
+    return (typeof path === 'undefined') ? store : _.get(store, path, defaultValue);
   }
-  return getSetFactory(_.get(store, path));
-};
+}
+
+function setter(store) {
+  return (path, value) => {
+    return _.set(store, path, value);
+  }
+}
+
+function hasChecker(store) {
+  return path => {
+    return _.has(store, path);
+  }
+}
+
+function pusher(store) {
+  return value => {
+    return store.push(value);
+  }
+}
+
+function storeCreator(store) {
+  return (path, initial={}) => {
+    if (!_.has(store, path)) {
+      _.set(store, path, initial);
+    }
+    return getSetFactory(_.get(store, path));
+  }
+}
 
 function getSetFactory(store=rootStore) {
   return Object.freeze({
     store: storeCreator(store),
     get: getter(store),
     set: setter(store),
-    has: haser(store),
+    has: hasChecker(store),
     push: pusher(store),
-    cachedResult: (key, creator, options={}) => cachedResult(getter(store), setter(store), haser(store), key, creator, options)
+    cachedResult: (key, creator, options={}) => cachedResult(getter(store), setter(store), hasChecker(store), key, creator, options)
   });
 }
 
 module.exports = getSetFactory;
-
-
